c4a589b5a26cd94ead9aa4f6130ea4061b2253aa
Aden Herold <aden.herold1@gmail.com>
2019-06-04 11:10:09 +0930

    Added long description to setup.py to be read from the project README.md markdown file

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
 import setuptools
 
+with open('README.md') as file:
+    long_description = file.read()
+
 setuptools.setup(
     name="typemallow",
     version="0.0.1",
@@ -10,6 +13,8 @@ setuptools.setup(
     author_email="aden.herold1@gmail.com",
 
     description="An elegant and automatic solution for generating/outputting Typescript interfaces from your Marshmallow Schemas.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords=['Marshmallow', 'Typescript', 'Python', 'Flask', 'Django'],
 
     packages=['typemallow'],

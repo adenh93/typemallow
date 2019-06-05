0a535d5a2f80eb37510d12a40533b3eff499f10a
Aden Herold <aden.herold1@gmail.com>
2019-06-05 19:54:44 +0930

    bumped package version to 0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 
 setuptools.setup(
     name="typemallow",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/adenh93/typemallow",
 
     author="Aden Herold",

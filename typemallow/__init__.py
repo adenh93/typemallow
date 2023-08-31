83ea8dbaef66721563aabeea35d044de5ac2cbca
Niels van Schooten <nvs@smartloxs.com>
2023-08-31 13:26:51 +0200

    - added naming convention option

--- typemallow/__init__.py
+++ typemallow/__init__.py
@@ -3,10 +3,11 @@ from .mappings import mappings
 
 __schemas = dict()
 
-def ts_interface(context='default'):
-    '''
 
-    Any valid Marshmallow schemas with this class decorator will 
+def ts_interface(context='default'):
+    """
+
+    Any valid Marshmallow schemas with this class decorator will
     be added to a list in a dictionary. An optional parameter: 'context'
     may be provided, which will create separate dictionary keys per context.
     Otherwise, all values will be inserted into a list with a key of 'default'.
@@ -17,25 +18,32 @@ def ts_interface(context='default'):
     class Foo(Schema):
         first_field = fields.Integer()
 
-    '''
+    """
+
     def decorator(cls):
         if issubclass(cls, Schema):
             if not context in __schemas:
                 __schemas[context] = []
             __schemas[context].append(cls)
         return cls
+
     return decorator
 
 
-def __get_ts_interface(schema):
-    '''
+def __camelcase(s):
+    parts = iter(s.split("_"))
+    return next(parts) + "".join(i.title() for i in parts)
+
+
+def __get_ts_interface(schema, naming_convention):
+    """
 
     Generates and returns a Typescript Interface by iterating
     through the declared Marshmallow fields of the Marshmallow Schema class
     passed in as a parameter, and mapping them to the appropriate Typescript
     data type.
 
-    '''
+    """
     name = schema.__name__.replace('Schema', '')
     ts_fields = []
     for key, value in schema._declared_fields.items():
@@ -46,25 +54,30 @@ def __get_ts_interface(schema):
         else:
             ts_type = mappings.get(type(value), 'any')
 
-        ts_fields.append(
-            f'\t{key}: {ts_type};'
-        )
+        if naming_convention:
+            ts_fields.append(
+                f'\t{__camelcase(key)}: {ts_type};'
+            )
+        else:
+            ts_fields.append(
+                f'\t{key}: {ts_type};'
+            )
     ts_fields = '\n'.join(ts_fields)
     return f'export interface {name} {{\n{ts_fields}\n}}\n\n'
 
 
-def generate_ts(output_path, context='default'):
-    '''
+def generate_ts(output_path, context='default', naming_convention=False):
+    """
 
     When this function is called, a Typescript interface will be generated
     for each Marshmallow schema in the schemas dictionary, depending on the
     optional context parameter provided. If the parameter is ignored, all
     schemas in the default value, 'default' will be iterated over and a list
     of Typescript interfaces will be returned via a list comprehension.
-    
+
     The Typescript interfaces will then be outputted to the file provided.
 
-    '''
+    """
     with open(output_path, 'w') as output_file:
-        interfaces = [__get_ts_interface(schema) for schema in __schemas[context]]
+        interfaces = [__get_ts_interface(schema, naming_convention) for schema in __schemas[context]]
         output_file.write(''.join(interfaces))
